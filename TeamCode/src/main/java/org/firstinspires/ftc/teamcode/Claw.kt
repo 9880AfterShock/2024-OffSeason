0cee22681e5cc33ab88028bff505a654cb69304e
DaBiggestBozo <kraaisam000@edmonds15.org>
2023-11-17 13:49:19 -0800

    basic 2.0 walle arm code thingie wheee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.kt
@@ -50,9 +50,9 @@ object Claw : Subsystem {
 
     val clawServo = ServoEx("Claw")
     @JvmField
-    var CLOSE_POSITION = 0.5 // old 0.75
+    var CLOSE_POSITION = 0.0 // old 0.75
     @JvmField
-    var OPEN_POSITION = 0.0 // old 0.5
+    var OPEN_POSITION = 0.5 // old 0.5t
     val Switch: Command
         get() = parallel {
             if (ClawState == "Closed") {
