f2b9649d92e76a42658160e761bb8e4e8ad7014f
Morriswell <pioltoscarrothwell@gmail.com>
2024-03-07 14:27:33 -0800

    Finnished one of the Trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/primary/TrajectoryFactory.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/primary/TrajectoryFactory.kt
@@ -9,20 +9,36 @@ object TrajectoryFactory {
     // Poses
     lateinit var startDropSide : Pose2d
     lateinit var scoreCenterDropSide : Pose2d
+    lateinit var centerScorePose : Pose2d
+    lateinit var parkPose : Pose2d
     // Trajectories
     lateinit var startToCenterDropSide : Trajectory
     lateinit var centerToBackDropSide : Trajectory
+    lateinit var centerBackToScore : Trajectory
+    lateinit var scoreCenterToBackup : Trajectory
+    lateinit var centerBackupToPark : Trajectory
     fun initTrajectories(drive : SampleMecanumDrive) {
         //put pos here
         startDropSide = Pose2d(9.0,61.6,-PI/2)
         scoreCenterDropSide = Pose2d(12.0, 31.0, -PI/2)
+        centerScorePose = Pose2d(48.0, 36.0, 0.0)
+        parkPose = Pose2d(58.0, 13.0, -PI/8)
 
         //put trajectories here
         startToCenterDropSide = drive.trajectoryBuilder(startDropSide)
             .splineToSplineHeading(scoreCenterDropSide, -PI/2)
             .build()
         centerToBackDropSide = drive.trajectoryBuilder(scoreCenterDropSide, 0.0)
-            .back(1.0)
+            .back(16.0)
+            .build()
+        centerBackToScore = drive.trajectoryBuilder(centerToBackDropSide.end(), -PI/2)
+            .splineToSplineHeading(centerScorePose, 0.0)
+            .build()
+        scoreCenterToBackup = drive.trajectoryBuilder(centerScorePose, 0.0)
+            .back(12.0)
+            .build()
+        centerBackupToPark = drive.trajectoryBuilder(scoreCenterToBackup.end(), -PI/2)
+            .splineToSplineHeading(parkPose, 0.0)
             .build()
 // todo Create switch color, and add .switch color.
 
